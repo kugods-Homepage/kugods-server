e5ccab46336cd5943d0c74632a585f877f49f310
worjs <whworjs777@gmail.com>
2022-11-16 00:40:10 +0900

    global class validator init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // class validator 세팅
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      skipUndefinedProperties: true,
+    }),
+  );
+
+  // swagger 세팅
   const config = new DocumentBuilder()
     .setTitle('KUGODS Server')
     .setDescription('KUGODS API description')
