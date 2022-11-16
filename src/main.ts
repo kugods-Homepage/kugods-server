ca95eec681d383dd03f9e11b26c04fde883c4cdc
JaeGun Cho <whworjs777@gmail.com>
2022-11-16 23:54:02 +0900

    [Feat] prisma, swagger, validator, 예시 api 세팅 (#7)
    * add common module with prisma service
    
    * init swagger
    
    * global class validator init
    
    * init user module & test api sample

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // class validator 세팅
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      skipUndefinedProperties: true,
+    }),
+  );
+
+  // swagger 세팅
+  const config = new DocumentBuilder()
+    .setTitle('KUGODS Server')
+    .setDescription('KUGODS API description')
+    .setVersion('1.0')
+    .addTag('KUGODS')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
