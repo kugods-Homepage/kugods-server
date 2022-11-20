b386855c8a21910e51e011e29f9557f3725f9303
woog2roid <woog2roid@gmail.com>
2022-11-20 16:38:11 +0000

    Rename(UserPosition): .enum -> .type

---
+++ src/user/types/user-position.type.ts
@@ -0,0 +1,6 @@
+export enum UserPosition {
+  '리드' = 'LAED',
+  '코어' = 'CORE',
+  '멤버' = 'MEMBER',
+  '주니어' = 'JUNIOR',
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,6 +5,8 @@ import { TestDto } from './dto/test.dto';
 import { TestType } from './types/test.type';
 import * as XLSX from 'xlsx';
 import { XlsxEnrollDao } from './dao/xlsx-enroll.dao';
+import { UserPosition } from './types/user-position.type';
+import { validateOrReject } from 'class-validator';
 
 @Injectable()
 export class UserService {
@@ -23,6 +25,8 @@ export class UserService {
   }
 
   async enrollByXlsx(file: Express.Multer.File): Promise<void> {
+    console.log(file);
+
     // 엑셀 데이터 가져오기
     const workbook = XLSX.read(file.buffer, { type: 'buffer' });
     const sheetName = workbook.SheetNames[0];
@@ -32,25 +36,31 @@ export class UserService {
     });
     console.log(xlsxRows);
 
-    // DAO 형식으로 맞추기
-    const xlsxEnrollData: XlsxEnrollDao[] = [];
+    // XLSX -> JSON DAO 형식으로 맞추기 (with validtaion)
+    const xlsxEnrollDataArray: XlsxEnrollDao[] = [];
     for (const row of xlsxRows) {
       const values = Object.keys(row).map((key) => row[key]);
       const [name, studentId, phone, xlsxPosition] = values;
 
-      const data = new XlsxEnrollDao(name, studentId, phone);
+      if (!(xlsxPosition === '리드' || xlsxPosition === '코어' || xlsxPosition === '멤버' || xlsxPosition === '주니어'))
+        throw new BadRequestException('회원 구분이 잘못되었습니다.');
 
-      if (xlsxPosition === '리드') data.setPosition('LEAD');
-      else if (xlsxPosition === '코어') data.setPosition('CORE');
-      else if (xlsxPosition === '멤버') data.setPosition('MEMBER');
-      else if (xlsxPosition === '주니어') data.setPosition('JUNIOR');
-      else throw new BadRequestException('회원 구분이 잘못되었습니다.');
+      const xlsxEnrollData: XlsxEnrollDao = {
+        name: name,
+        studentId: studentId,
+        phone: phone,
+        position: UserPosition[xlsxPosition],
+      };
 
-      console.log(data);
-      xlsxEnrollData.push(data);
+      console.log(xlsxEnrollData);
+      validateOrReject(xlsxEnrollData).catch(() => {
+        throw new BadRequestException('엑셀 파일의 정보에 에러가 있습니다.');
+      });
+
+      xlsxEnrollDataArray.push(xlsxEnrollData);
     }
 
     // DB 등록하기
-    return this.userRepository.enroll(xlsxEnrollData);
+    return this.userRepository.enroll(xlsxEnrollDataArray);
   }
 }
