6b1536613afe391231ac6c6337a539f1e55f960a
woog2roid <woog2roid@gmail.com>
2022-12-27 18:34:34 +0900

    feat(/auth/join): 기가입된 유저가 가입 시도할 경우에 예외처리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { JoinPayload } from './payload/join.payload';
 import * as bcrypt from 'bcrypt';
 import { UserRepository } from 'src/user/user.repository';
 import { UserService } from 'src/user/user.service';
+//import { LoginPayload } from './payload/login.payload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userRepository: UserRepository, private readonly userService: UserService) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async joinEnrolledUser(payload: JoinPayload) {
     const { email, password, studentId, accessCode } = payload;
@@ -19,12 +25,19 @@ export class AuthService {
       });
     }
 
-    // userId 받아오기
-    const { id: userId } = await this.userRepository.getUserIdByStudentId(studentId);
+    // 기가입된 계정 오류 처리
+    if (this.userRepository.getUserAccountByEmail(email)) {
+      throw new ConflictException({
+        statusCode: 409,
+        message: `이미 가입된 이메일입니다.`,
+        error: 'Conflict',
+      });
+    }
 
+    // userId 받아오기
+    const { id: userId } = await this.userRepository.getUserByStudentId(studentId);
     // password 암호화
     const hashedPassword = await bcrypt.hash(password, 12);
-
     // DB에 등록
     return this.userRepository.joinEnrolledUser(userId, email, hashedPassword);
   }
