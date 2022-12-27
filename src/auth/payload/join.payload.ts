aeaf593c8c6c5da6cad4af757a827825d4bb6056
woog2roid <woog2roid@gmail.com>
2022-12-27 18:36:43 +0900

    feat(join payload): IsDefined와 필수(!) 추가

--- src/auth/payload/join.payload.ts
+++ src/auth/payload/join.payload.ts
@@ -1,24 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsNumber, IsString, Max, Min } from 'class-validator';
+import { IsDefined, IsEmail, IsNotEmpty, IsNumber, IsString, Max, Min } from 'class-validator';
 
 export class JoinPayload {
   @ApiProperty({ type: String })
+  @IsDefined()
   @IsNotEmpty()
   @IsEmail()
-  public email: string;
+  public email!: string;
 
   @ApiProperty({ type: String })
+  @IsDefined()
   @IsNotEmpty()
-  public password: string;
+  @IsString()
+  public password!: string;
 
   @ApiProperty({ type: Number })
+  @IsDefined()
   @IsNumber()
   @Min(1000000000)
   @Max(9999999999)
-  public studentId: number;
+  public studentId!: number;
 
   @ApiProperty({ type: String })
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
-  public accessCode: string;
+  public accessCode!: string;
 }
