1da766ef2604b42911a242d740f48d335a162ecd
woog2roid <woog2roid@gmail.com>
2023-01-04 14:46:51 +0900

    feat: controller에 적용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Post, Body, HttpCode, UseInterceptors, UploadedFile, Get, Query } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, UseInterceptors, UploadedFile, Get, Query, UseGuards } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBody, ApiConsumes, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JoinMemberPayload } from 'src/auth/payload/join-member.payload';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { LoginPayload } from './payload/login.payload';
 import { xlsxEnrollOption } from 'src/utils/multer/xlsx-enroll.option';
 import { CheckEmailPayload } from './payload/check-email.payload';
+import { JwtAuthGuard } from './guard/jwt-atuh.guard';
 
 @ApiTags('Auth API')
 @Controller('auth')
@@ -14,6 +23,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @ApiOperation({ summary: '합격자 정보가 담긴 엑셀 파일 업로드를 통해 서버에 정보 등록' })
+  @ApiBearerAuth()
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     schema: {
@@ -27,6 +37,7 @@ export class AuthController {
     },
   })
   @ApiCreatedResponse()
+  @UseGuards(JwtAuthGuard)
   @HttpCode(201)
   @Post('/enroll')
   @UseInterceptors(FileInterceptor('file', xlsxEnrollOption))
@@ -35,7 +46,9 @@ export class AuthController {
   }
 
   @ApiOperation({ summary: '유저들의 승인코드가 담긴 xlsx파일을 base64형태로 생성해서 내려줌' })
+  @ApiBearerAuth()
   @ApiOkResponse()
+  @UseGuards(JwtAuthGuard)
   @HttpCode(200)
   @Get('/access-code')
   async generateXlsxWithAccessCode(): Promise<string> {
